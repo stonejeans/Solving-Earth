92afce78a373ec36ebcd12682fad24c900984144
Shaun Bristow <91874480+stonejeans@users.noreply.github.com>
2022-06-07 15:45:00 +0200

    Update Project File - Refactor
    - Add indentation to longer statements to improve readability.
    - Update dtype conversion method used from astype to applymap.

--- se-pandas-numpy-opt.py
+++ se-pandas-numpy-opt.py
@@ -2,9 +2,6 @@ import time
 import numpy as np
 import pandas as pd
 
-MY_COLUMN_VALS = ["a" ,"e" , "h", "n", "o", "r", "s", "t", "u", "w"]
-MY_COLUMN_WORDS = ['north', 'west', 'south', 'east','earth']
-
 
 def mpermutations(n):
     '''NumPy solution that builds the permutations of size m 
@@ -24,35 +21,50 @@ def mpermutations(n):
 
 def create_dataframes():
     '''Create DataFrames using the Pandas library'''
+    MY_COLUMN_VALS = ["a" , "e", "h", "n", "o", "r", "s", "t", "u", "w"]
+    MY_COLUMN_WORDS = ['north', 'west', 'south', 'east','earth']
+    
     my_np_array = mpermutations(10)
-    
     char_val_df = pd.DataFrame(data = my_np_array, columns=MY_COLUMN_VALS)
-    char_val_df.drop(char_val_df[(char_val_df.n > char_val_df.e) | (char_val_df.s > char_val_df.e)].index, inplace=True)
-    
-    for char in MY_COLUMN_VALS:
-         char_val_df[char]= char_val_df[char].astype(str)
+    char_val_df.drop(
+        char_val_df[
+            (char_val_df.n >= char_val_df.e) | (char_val_df.s >= char_val_df.e)
+            ].index, inplace=True
+        )
+    char_val_df= char_val_df.applymap(str)
 
-    word_val_df = pd.DataFrame(data = char_val_df)
-
+    word_val_df = pd.DataFrame(data = char_val_df, columns= MY_COLUMN_WORDS)
     # Concatenate specific column values (Strings only)
-    word_val_df['north'] = char_val_df['n'] + char_val_df['o'] + char_val_df['r'] + char_val_df['t'] + char_val_df['h']
-    word_val_df['west'] = char_val_df['w'] + char_val_df['e'] + char_val_df['s'] + char_val_df['t']
-    word_val_df['south'] = char_val_df['s'] + char_val_df['o'] + char_val_df['u'] + char_val_df['t'] + char_val_df['h']
-    word_val_df['east'] = char_val_df['e'] + char_val_df['a']+ char_val_df['s'] + char_val_df['t']
-    word_val_df['earth'] = char_val_df['e'] + char_val_df['a'] + char_val_df['r'] + char_val_df['t'] + char_val_df['h']
-
-    for word in MY_COLUMN_WORDS:
-        word_val_df[word] = word_val_df[word].astype(int)
+    word_val_df['north'] = char_val_df['n'] + char_val_df['o'] \
+        + char_val_df['r'] + char_val_df['t'] + char_val_df['h']
     
+    word_val_df['west'] = char_val_df['w'] + char_val_df['e'] + char_val_df['s'] + char_val_df['t']
+    
+    word_val_df['south'] = char_val_df['s'] + char_val_df['o'] \
+        + char_val_df['u'] + char_val_df['t'] + char_val_df['h']
+    
+    word_val_df['east'] = char_val_df['e'] + char_val_df['a']+ char_val_df['s'] + char_val_df['t']
+    
+    word_val_df['earth'] = char_val_df['e'] + char_val_df['a'] + char_val_df['r'] \
+        + char_val_df['t'] + char_val_df['h']
+
+    word_val_df= word_val_df.astype(int)
+
+    word_val_df.drop(word_val_df[(word_val_df.earth < word_val_df.north + word_val_df.south + word_val_df.east + word_val_df.west)].index, inplace=True)
+
     return word_val_df
 
 def equation_test(word_val_df):
     '''Use numpy to sum values and test if equation holds true'''
     word_val_df['earth_test'] = (word_val_df['north'].to_numpy() \
         + word_val_df['west'].to_numpy() + word_val_df['south'].to_numpy() \
-        + word_val_df['east'].to_numpy()).tolist()
+        + word_val_df['east'].to_numpy())
 
-    word_val_df.drop(word_val_df[~(word_val_df.earth == word_val_df.earth_test)].index, inplace=True)
+    word_val_df.drop(
+        word_val_df[
+            ~(word_val_df.earth == word_val_df.earth_test)
+            ].index, inplace=True
+        )
     
     df_solution_output = f"Solutions DataFrame: \n{word_val_df}"
     df_datashape = f"\nData Shape: \n{word_val_df.shape}\n"
@@ -61,7 +73,8 @@ def equation_test(word_val_df):
     df_max_val = f"Max Value: {max_val}"
 
     print(df_solution_output,df_datashape,df_max_val)
-
-#-- Driver Code --#
+    
+#-------- Driver Code With Runtime Test --------#
 word_val_df = create_dataframes()
 equation_solve = equation_test(word_val_df)
+
